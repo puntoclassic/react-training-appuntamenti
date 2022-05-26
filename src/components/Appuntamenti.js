10b350cc5774536b90986407ab1e41eea7002ec6
Jonathan La Mela <jonathan.la.mela@gmail.com>
2022-05-26 09:28:08 +0200

    rimozione console.log non necessari

--- src/components/Appuntamenti.js
+++ src/components/Appuntamenti.js
@@ -3,11 +3,15 @@ import AddListItem from "./AddListItem";
 import RenderList from "./RenderList";
 
 export default function Appuntamenti() {
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState([]); //array vuoto
 
+    //aggiunti un elemento all'array items
     const addItemAction = (item) => {
         setItems((oldList) => {
+            //va creata una nuova lista altrimenti il setState non fa il re-rendering
             var newList = [...oldList];
+
+            //aggiunge il nuovo elemento nella copia della lista vecchia
             newList.push(item);
             return newList;
         });
@@ -15,11 +19,13 @@ export default function Appuntamenti() {
 
     const deleteItemAction = (item) => {
         setItems((oldList) => {
+            //crea una nuova lista
             var newList = [...oldList];
+
+            //restituisce una nuova lista escludendo l'elemento passato come parametro
             return newList.filter((i) => i.descrizione !== item.descrizione && i.dipendente !== item.dipendente && i.orario !== item.orario);
         });
     };
-    console.log("Rebuild");
 
     return (
         <>
